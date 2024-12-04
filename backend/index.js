eb6fdfbea3f926ed16632c13cff7af37dfc304ed
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-12-04 16:05:44 +0600

    added mongodb, mongoose setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://roshidmamun2024:<dqjdzRXbEgWOnL68>@cluster0.1kfb0.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected!"));
 
 app.post("/navbar", function (req, res) {
   //   res.send("Hello World");
-  console.log("navbar");
+  console.log(req.body);
 });
 
 app.listen(8000);
